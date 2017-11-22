e4c58f7ce898ec6ff2912c5ef6a3bc516ba0d0de
Brad Hambrice <hambrice@uab.edu>
2017-11-22 02:55:53 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function theBeatlesPlay (musicians, instruments) {
+  var beatlesLoops = [];
+  for (var i=0; i < musicians.length; i++) {
+    beatlesLoops.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+    return beatlesLoops
+}
+function johnLennonFacts (facts) {
